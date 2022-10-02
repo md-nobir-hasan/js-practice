6e96a3fd4318b083c55ccbf9a7411f9d55fed818
Md. Nobir Hasan <nobir.wd.1@gmail.com>
2022-10-02 19:35:55 +0600

    validation done

---
+++ escape_notation.js
@@ -0,0 +1,9 @@
+//escape notation use করা হয় js এর প্রিডিগফাইন name, character,sign etc প্রিন্ট করার জন্য
+var escape_notaion = 'Subject \' ASVM \' is ver hard';
+console.log(escape_notaion);
+escape_notaion = 'I am \n nobir'; //n for new line
+console.log(escape_notaion);
+
+escape_notaion = 'backslash print \\'
+console.log(escape_notaion);
+//for more search in google
