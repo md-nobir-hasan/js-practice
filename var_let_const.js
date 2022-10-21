2af9db10a5376cf616a44c8c2e6513aab914c0e4
Md. Nobir Hasan <nobir.wd.1@gmail.com>
2022-10-21 19:00:00 +0600

    differences between var,let and const

---
+++ var_let_const.js
@@ -0,0 +1,20 @@
+   // age= 24; variable declare ar somoy kicu na dile 1.fucntional scope(sudu function er vitor diclare korle ar baire pabe na) 2.hoist er subidha nai; mane declare er age access kora jabe na; 3. value change kora jai; 
+   //var age = 40;1. functional scope(sudu function er vitor diclare korle ar baire pabe na); 2. hoisting subidha ace(ai aktatei sudu hoisting subidha ace) 3. redeclare kora jai; 4. value change kora jai
+   //let age = 50; 1. block scope (je kono  second bracket er vitor declare korle ar vaire pabe na) 2.hoisting subidha nai; 3.redeclare kora jai na, 3. redeclare kora jai na; 4. vlaue change kora jai
+   // const age =85;1.block scope  2.hoisting subidha nai; 3. value change kora jai na (constant); 4.redeclare kora jai na
+
+
+    age =25;
+
+
+for(i=0;i<1; i++){
+    // console.log(name1,age,height);
+    age=520;
+}
+function check(){
+    
+}
+console.log(age);
+check();
+
+
