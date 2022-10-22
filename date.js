626d5cb0bad41c42428f6bf48c7bf7519c1d5a5f
Md. Nobir Hasan <nobir.wd.1@gmail.com>
2022-10-22 17:16:36 +0600

    Create date.js

---
+++ date.js
@@ -0,0 +1,10 @@
+var date = new Date();
+console.log(date.toDateString());
+console.log(date.toTimeString());
+console.log(date.getFullYear());
+console.log(date.getMonth());
+console.log(date.getDate());
+console.log(date.getDay());
+console.log(date.getHours());
+console.log(date.getMinutes());
+console.log(date.getSeconds());
